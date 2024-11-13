591987ac56fdea94c69c38347371fa7bcf15466d
Huseyin <benhuseyingundogdu@gmail.com>
2024-11-13 01:48:41 +0100

    1st class completed

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // State to manage background color, starting with white
+  const [bgColor, setBgColor] = useState('#FFFFFF');
+
+  // Func to generate new random color
+  const generateRandomBgColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setBgColor(randomColor);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {backgroundColor: bgColor}]}>
+      <Text style={{marginVertical: 15}}>Hello World!</Text>
+
+      <Button
+        onPress={generateRandomBgColor}
+        title="Click me!"
+      />
+      
+      <Pressable 
+        style={styles.customButton} 
+        onPress={generateRandomBgColor}>
+
+        <Text>Custom Click me!</Text>
+
+      </Pressable>
+
+      <StatusBar style="auto" hidden={true}/>
     </View>
   );
 }
@@ -17,4 +41,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  customButton: {
+    marginVertical: 15,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3
+  },
+
 });
